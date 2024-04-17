0dbc139c6c328d87e12ec7a9b036ebe344eebfea
paulus <paulusojak@gmail.com>
2024-04-17 12:11:52 +0700

    Add buttons

---
+++ scripts.js
@@ -0,0 +1,15 @@
+const add = function(a, b){
+    return a + b;
+}
+
+const sub = function(a, b){
+    return a - b;
+}
+
+const multiply = function(a, b){
+    return a*b;
+}
+
+const divide = function(a, b){
+    return a/b;
+}
