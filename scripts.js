bb9baf0db74cfd5cda231562a149f122b87c76b1
paulus <paulusojak@gmail.com>
2024-04-18 10:06:53 +0700

    Add new buttons and functions

--- scripts.js
+++ scripts.js
@@ -7,9 +7,32 @@ const sub = function(a, b){
 }
 
 const multiply = function(a, b){
-    return a*b;
+    return a * b;
 }
 
 const divide = function(a, b){
-    return a/b;
+    return a / b;
+}
+
+const operate = {
+    '+' : add,
+    '-' : sub,
+    '*' : multiply,
+    '/' : divide
+}
+
+//buttons declaration
+const btnNumbers = document.querySelectorAll(".numbers");
+const btnOpp = document.querySelectorAll(".operators");
+
+
+//loop to wait until two operation is used
+let op = 0;
+
+if(op = 0){
+    btnNumbers.forEach((button) => {
+        button.addEventListener("click", () => {
+            alert(button.id);
+        });
+    });
 }
