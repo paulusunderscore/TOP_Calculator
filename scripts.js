6d6844e7df421efcc5c5c22a2836d3d50a2f5f52
paulus <paulusojak@gmail.com>
2024-04-20 09:49:20 +0700

    Add variables for 1st & 2nd numbers and operators

--- scripts.js
+++ scripts.js
@@ -28,11 +28,36 @@ const btnOpp = document.querySelectorAll(".operators");
 
 //loop to wait until two operation is used
 let op = 0;
+let var1 = [];
+let varOp = [];
+let var2 = [];
 
-if(op = 0){
-    btnNumbers.forEach((button) => {
-        button.addEventListener("click", () => {
+console.log('oye');
+btnNumbers.forEach((button) => {
+    button.addEventListener("click", () => {
+        if(op!=0){
+            var2 = var2.concat(button.id);
             alert(button.id);
-        });
+            console.log('var2: '+var2);
+        }
+        else {
+            var1 = var1.concat(button.id);
+            alert(button.id);
+            console.log('var1: '+var1);
+        }
     });
-}
+});
+
+console.log('oye');
+btnOpp.forEach((button) => {
+    button.addEventListener("click", () => {
+        varOp = varOp.concat(button.id);
+        alert(button.id);
+        console.log('varOp: '+varOp);
+        //add operator count
+        op += 1;
+    });
+});
+
+
+
