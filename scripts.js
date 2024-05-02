b24c6bb869e573c073829ecce7c728901c5897de
paulus <paulusojak@gmail.com>
2024-05-02 18:44:17 +0700

    Add the equal button function to calculate

--- scripts.js
+++ scripts.js
@@ -1,4 +1,4 @@
-const { equal } = require("assert");
+//test: vtes = ['4', '+', '10', '-', '7']
 
 const operate = {
     '+' : (a, b) => a + b,
@@ -16,23 +16,23 @@ const equalBtn = document.querySelector(".equal");
 const display = document.querySelector(".calc-display");
 
 //loop to wait until two operation is used
-let op = 0;
-let var1 = [];
-let varOp = [];
-let var2 = [];
+let op = [];
+let var1;
+let var2;
 let result;
-let userInput = [];
+let userInput = '';
 let displayText = '';
+let data = [];
 
 btn.forEach((button) => {
     button.addEventListener("click", () => {
-        userInput = userInput.concat(button.id);
+        userInput += String(button.id);
         console.log(userInput);
         //check if user used multiply or divide symbol for better visual
-        if(button.id === '*'){
+        if(button.id == '*'){
             display.textContent += 'x';
         }
-        else if(button.id === '/'){
+        else if(button.id == '/'){
             display.textContent += ':';
         }
         else {
@@ -42,6 +42,28 @@ btn.forEach((button) => {
 });
 
 equalBtn.addEventListener("click", () => {
+    data = userInput.match(/[0-9]+|[^0-9]/g);
+
+    for(let i = 0; i < data.length; i++){
+        if(i == 0 || i % 2 == 0){
+            if(var1 == null){
+                var1 = Number(data[i]);
+                console.log('var1 : ', var1);
+            }
+            var2 = Number(data[i+2]);
+            console.log('var2 : ', var2);
+                             
+        }
+
+        if(i % 2 != 0){
+            op = data[i];
+            console.log('op : ', op);
+            var1 = operate[op](var1, var2);
+            console.log(var1, op, var2);
+        }
+    }
+    console.log('result : ', var1);
+    return var1;
     
 });
 
